2b62613b98e51fd7d7342abfb7d480369bec0191
sebastianschroh <32150363+sebastianschroh@users.noreply.github.com>
2018-04-04 16:10:51 -0600

    LoginWindow, Client, Server
    These are new updates to loginWindow, Client and server.
    2018-04-14

--- src/frontend/LoginWindow.java
+++ src/frontend/LoginWindow.java
@@ -1,15 +1,37 @@
 package frontend;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.EmptyBorder;
-import java.io.*;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Component;
 
+<<<<<<< HEAD
+public class LoginWindow {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow window = new LoginWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+=======
 public class LoginWindow extends JFrame{
 	
 	public LoginWindow()
@@ -22,15 +44,15 @@ public class LoginWindow extends JFrame{
 		center.setBorder(new EmptyBorder(20,20,20,20));
 		center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
 		JLabel d3l = new JLabel(new ImageIcon("C:/School/softeng_yr2/ENSF409/TermProject/409Project/Desire3Learn.png"));
-		center.add(d3l, Component.CENTER_ALIGNMENT);
+		center.add(d3l);
 		this.add(center, BorderLayout.CENTER);
 		//center.add(Box.createVeritcalGlue());
 		JPanel pane1 = new JPanel();
 		pane1.setLayout(new FlowLayout());
 		pane1.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		pane1.add(new JLabel("Username: "), Component.LEFT_ALIGNMENT);
+		pane1.add(new JLabel("Username: "));
 		pane1.add(new JTextField(20));
-		center.add(pane1, Component.BOTTOM_ALIGNMENT);
+		center.add(pane1);
 		center.add(Box.createHorizontalGlue());
 		this.add(center, BorderLayout.CENTER);
 		addWindowListener(new java.awt.event.WindowAdapter() {
@@ -40,15 +62,55 @@ public class LoginWindow extends JFrame{
 						"Exit Application", JOptionPane.YES_NO_OPTION);
 				if (result == JOptionPane.YES_OPTION)
 					setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+>>>>>>> f82842e94348e8c45da86a9cac9441afed42c2c2
 			}
 		});
-		
 	}
-	
-	public static void main(String [] args)
-	{
-		LoginWindow yeet = new LoginWindow();
-		yeet.setVisible(true);
+
+	/**
+	 * Create the application.
+	 */
+	public LoginWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 497, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(30, 140, 62, 13);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(30, 163, 62, 13);
+		frame.getContentPane().add(lblPassword);
+		
+		textField = new JTextField();
+		textField.setBounds(102, 137, 179, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnLogin.setBounds(291, 136, 125, 40);
+		frame.getContentPane().add(btnLogin);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(102, 160, 179, 19);
+		frame.getContentPane().add(passwordField);
+		
+		JLabel lblDesirelearn = new JLabel("Desire3Learn");
+		lblDesirelearn.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblDesirelearn.setForeground(Color.ORANGE);
+		lblDesirelearn.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblDesirelearn.setBounds(102, 31, 237, 40);
+		frame.getContentPane().add(lblDesirelearn);
 	}
 }
---
+++ src/shareddata/StudentEnrollment.java
@@ -0,0 +1,50 @@
+package shareddata;
+
+import java.io.Serializable;
+
+public class StudentEnrollment implements Serializable{
+	
+	private int id;
+	private int student_id;
+	private int course_id;
+	private boolean enrolling;
+	
+	public StudentEnrollment(int id, int sid, int cid, boolean enr){
+		this.id = id;
+		student_id = sid;
+		course_id = cid;
+		enrolling = enr;
+	}
+	
+	public void setId(int i){
+		id = i;
+	}
+	
+	public void setStudentID(int i){
+		student_id = i;
+	}
+	
+	public void setCourseID(int i){
+		course_id = i;
+	}
+	
+	public void setEnrollment(boolean b){
+		enrolling = b;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public int getStudentID(){
+		return student_id;
+	}
+	
+	public int getCourseID(){
+		return course_id;
+	}
+	
+	public boolean getEnrollment(){
+		return enrolling;
+	}
+}
