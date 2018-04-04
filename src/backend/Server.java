dce4209fd6ce7941d9ca5e2148bc8ecce61dc9ca
Zach Sims <zach.md.sims97@gmail.com>
2018-04-04 17:43:51 -0600

    server is getting database stuff

--- src/backend/Server.java
+++ src/backend/Server.java
@@ -1,5 +1,49 @@
 package backend;
 
-public class Server {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import shareddata.Student;
+
+public class Server {
+	private DatabaseHelper database;
+	
+	public Student searchStudentsID(int id){
+		
+		try {
+			database = new DatabaseHelper();
+			database.prepareStatement("SELECT * FROM termproject.user WHERE id = ?");
+			database.getStatement().setInt(1,  id);
+			
+			ResultSet rs = database.getStatement().executeQuery();
+			
+			if(rs.next() && rs.getString(6).charAt(0) == 'S'){
+				return new Student(rs.getInt(1), rs.getString(2), rs.getString(3));
+			}
+			else return new Student (0, null, null);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return new Student (0, null, null);
+	}
+	
+	public ArrayList<Student> searchStudentsName(String name){
+		
+		ArrayList<Student> list = new ArrayList<Student>();
+		try {
+			database = new DatabaseHelper();
+			database.prepareStatement("SELECT * FROM termproject.user WHERE lastname = ?");
+			database.getStatement().setString(1, name);
+			
+			ResultSet rs = database.getStatement().executeQuery();
+			
+			while(rs.next() && rs.getString(6).charAt(0) == 'S'){
+				list.add(new Student(rs.getInt(1), rs.getString(2), rs.getString(3)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
