fc141ab20b792d7b047366fb5d44b4f358261c85
Zach Sims <zach.md.sims97@gmail.com>
2018-04-06 11:52:55 -0600

    mornin fellas

---
+++ src/frontend/pages/Page.java
@@ -0,0 +1,10 @@
+package frontend.pages;
+
+import java.util.ArrayList;
+
+import javax.swing.Box;
+
+public class Page<T extends Box> {
+	protected boolean isProfessor;
+	protected ArrayList<T> list;
+}
