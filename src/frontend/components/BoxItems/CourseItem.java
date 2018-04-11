23254561e952d782c5d21a194476e8bbdfc2dc0c
Zach Sims <zach.md.sims97@gmail.com>
2018-04-11 15:11:23 -0600

    ButtonBitch

--- src/frontend/components/BoxItems/CourseItem.java
+++ src/frontend/components/BoxItems/CourseItem.java
@@ -13,13 +13,18 @@ import shareddata.Course;
 
 public class CourseItem extends BoxItem{
 	private Course course;
-	private JButton active, viewSubmissions, viewStudents, addStudents, viewAssignments, addAssignment, emailStudents;
+	private JButton active, viewStudents, addStudents, viewAssignments, addAssignment, emailStudents, emailProf;
 	private CourseItemListener listener;
 	
 	public CourseItem(Course c, boolean isProf) {
+		
 		super(c.getName());
 		course = c;
 		listener = new CourseItemListener(null, null);
+		viewAssignments= new JButton("View Assignments");
+		viewAssignments.addActionListener(listener);
+		add(viewAssignments);
+		
 		if(isProf == true){
 		active = new JButton("Set Active");
 		active.addActionListener(listener);
@@ -27,16 +32,24 @@ public class CourseItem extends BoxItem{
 			active.setForeground(Color.black);
 		else
 			active.setForeground(Color.red);
-		viewSubmissions = new JButton("View Submissions");
-		viewSubmissions.addActionListener(listener);
 		viewStudents = new JButton("View Students");
 		viewStudents.addActionListener(listener);
 		addStudents = new JButton("Add Students");
 		addStudents.addActionListener(listener);
+		addAssignment = new JButton("Add Assignment");
+		addAssignment.addActionListener(listener);
+		emailStudents = new JButton("Email Students");
+		emailStudents.addActionListener(listener);
+		add(emailStudents);
+		add(addAssignment);
 		add(active);
 		add(viewStudents);
-		add(viewSubmissions);
 		add(addStudents);
+		}
+		else{
+			emailProf = new JButton("Email Professor");
+			emailProf.addActionListener(listener);
+			add(emailProf);
 		}
 	}
 	public Course getCourse() {
