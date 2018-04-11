c50e972eb957ee296dbf7ac4dcbe8ed62e6d9028
Zach Sims <zach.md.sims97@gmail.com>
2018-04-11 14:31:40 -0600

    Yoink

--- src/frontend/pages/Page.java
+++ src/frontend/pages/Page.java
@@ -1,11 +1,7 @@
 package frontend.pages;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.util.ArrayList;
 
 import javax.swing.Box;
@@ -14,55 +10,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
 
 import shareddata.Assignment;
 
-public class Page extends JPanel {
-	private JLabel courseName;
-	private JPanel coursePanel;
+public class Page extends JFrame {
+	private JLabel name;
+	private JPanel listPanel;
+	private ArrayList<Assignment> assignments;
 	
 	public Page(String s){
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{61, 64, 277, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.WHITE);
-		panel_2.setLayout(null);
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.gridwidth = 2;
-		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 0;
-		add(panel_2, gbc_panel_2);
-		
-		JLabel lblLol = new JLabel(s);
-		lblLol.setBounds(10, 11, 548, 57);
-		lblLol.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLol.setFont(new Font("Comic Sans MS", Font.PLAIN, 22));
-		panel_2.add(lblLol);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.BLACK);
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.gridwidth = 2;
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 1;
-		add(panel_1, gbc_panel_1);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
-		gbc_scrollPane_1.gridwidth = 2;
-		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane_1.gridx = 0;
-		gbc_scrollPane_1.gridy = 2;
-		add(scrollPane_1, gbc_scrollPane_1);
 	}
 }
