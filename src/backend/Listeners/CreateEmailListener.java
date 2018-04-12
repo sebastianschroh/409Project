805daec0d27af0e57e37136999c2734d67987157
sebastianschroh <32150363+sebastianschroh@users.noreply.github.com>
2018-04-12 17:40:35 -0600

    yyeet pt2

--- src/backend/Listeners/CreateEmailListener.java
+++ src/backend/Listeners/CreateEmailListener.java
@@ -1,39 +1,5 @@
 package backend.Listeners;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import backend.EmailHelper;
-import frontend.components.PageNavigatorTest;
-import frontend.components.StudentEmailCreator;
-import frontend.components.BoxItems.CourseItem;
-import shareddata.Course;
-import shareddata.Email;
-import shareddata.Student;
-
-public class CreateEmailListener implements ActionListener {
-	
-	private CourseItem c;
-	private StudentEmailCreator e;
-	private PageNavigatorTest p;
-	
-	public CreateEmailListener(PageNavigatorTest p, CourseItem c, StudentEmailCreator e)
-	{
-		this.c = c;
-		this.e = e;
-		this.p = p;
-	}
-
-	public void actionPerformed(ActionEvent arg0) {
-		String subject = e.getSubject();
-		String message = e.getMessage();
-		ArrayList<String> list = c.getEmails();
-		Email email = new Email(null, list, subject, message);
-		p.sendObject(email);
-		p.sendObject("sendEmail");
-		
-		e.getFrame().dispose();
-	}
+public class CreateEmailListener {
 
 }
