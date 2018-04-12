5753039817b93b1a4b6b106d486725e6f9b5f992
Zach Sims <zach.md.sims97@gmail.com>
2018-04-12 17:05:05 -0600

    push it, bop it, twist it, pull it

--- src/frontend/components/StudentEmailCreator.java
+++ src/frontend/components/StudentEmailCreator.java
@@ -7,6 +7,8 @@ import java.awt.BorderLayout;
 import javax.swing.JTextPane;
 import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
+import java.awt.TextArea;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -18,7 +20,8 @@ import java.awt.event.ActionEvent;
 public class StudentEmailCreator {
 
 	private JFrame frame;
-
+	private JTextArea subjectArea;
+	private JTextArea messageArea;
 	/**
 	 * Launch the application.
 	 */
@@ -58,17 +61,17 @@ public class StudentEmailCreator {
 		lblSubject.setBounds(10, 16, 40, 14);
 		panel.add(lblSubject);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(60, 11, 364, 22);
-		panel.add(textArea);
+		subjectArea = new JTextArea();
+		subjectArea.setBounds(60, 11, 364, 22);
+		panel.add(subjectArea);
 		
 		JLabel lblMessage = new JLabel("Message:");
 		lblMessage.setBounds(4, 57, 46, 14);
 		panel.add(lblMessage);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(60, 55, 364, 176);
-		panel.add(textArea_1);
+		messageArea = new JTextArea();
+		messageArea.setBounds(60, 55, 364, 176);
+		panel.add(messageArea);
 		
 		JButton btnNewButton = new JButton("Send");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -77,5 +80,12 @@ public class StudentEmailCreator {
 		});
 		btnNewButton.setBounds(195, 238, 89, 23);
 		panel.add(btnNewButton);
+	}
+	
+	public String getSubject(){
+		return subjectArea.getText();
+	}
+	public String getMessage(){
+		return messageArea.getText();
 	}
 }
